b6fa2aa49598cb620e88523e912b8654603dfe3b
jehaann07carlos@gmail.com <jehaann07carlos@gmail.com>
2019-03-14 21:39:23 -0600

    improving styles on post page and index

--- index.php
+++ index.php
@@ -1,2 +1,45 @@
 <?php get_header();?>
+
+<section class="blog">
+<div class="container-blog-image" style="background-image: url('<?php echo RutaImagenes ?>/main-slider.png')">
+            <h1 class="titles-project">Entradas del Blog</h1>
+    </div>
+ <div class="container-blog">
+    <article class="blog-post">
+            <picture>
+                <a href="#"><img src="<?php echo RutaImagenes ?>/main-slider.png" alt="" class="img-post"></a>
+            </picture>
+            <div>
+                <h1 class="titles-project">Titulo del post</h1>
+                <p class="subtitles-project">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis voluptatum voluptates quis delectus quo reprehenderit nobis accusamus doloremque dolores id odio, totam modi eos fugit error earum dolore dignissimos dicta.</p>
+                <button class="btn">Leer mas</button>
+            </div>
+    </article>
+
+    <article class="blog-post">
+            <picture>
+                <a href="#"><img src="<?php echo RutaImagenes ?>/main-slider.png" alt="" class="img-post"></a>
+            </picture>
+            <div>
+                <h1 class="titles-project">Titulo del post</h1>
+                <p class="subtitles-project">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis voluptatum voluptates quis delectus quo reprehenderit nobis accusamus doloremque dolores id odio, totam modi eos fugit error earum dolore dignissimos dicta.</p>
+                <button class="btn">Leer mas</button>
+            </div>
+    </article>
+
+    <article class="blog-post">
+            <picture>
+                <a href="#"><img src="<?php echo RutaImagenes ?>/main-slider.png" alt="" class="img-post"></a>
+            </picture>
+            <div>
+                <h1 class="titles-project">Titulo del post</h1>
+                <p class="subtitles-project">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis voluptatum voluptates quis delectus quo reprehenderit nobis accusamus doloremque dolores id odio, totam modi eos fugit error earum dolore dignissimos dicta.</p>
+                <button class="btn">Leer mas</button>
+            </div>
+    </article>
+</div>
+</section>
+
+
+
 <?php get_footer();?>
