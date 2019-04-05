51eefdfcca6c0e4e2418e5785cb89ac8b9c91150
info@codecreativenicaragua.com <info@codecreativenicaragua.com>
2019-04-04 22:36:51 -0600

    add page full-width

--- template-fullwidth.php
+++ template-fullwidth.php
@@ -1,9 +1,17 @@
+<?php
+
+/** Template name: Ancho completo
+* PAGINA DE ANCHO COMPLETO
+@package wordpress
+@subpackage jehaann
+@since 1.0
+*/
+?>
+
 <?php get_header();?>
 
     <?php if (have_posts() ): while ( have_posts() ): the_post(); ?>
-        <section class="container-blog-image" style="background-image: url('<?php echo RutaImagenes ?>/main-slider.png')">
-           <h2></h2>
-        </section>
+        
         <section class="content-post">
             <article <?php post_class ('article single'); ?> id="post-<?php the_ID();?>">
                 <div class="article-content">
@@ -14,6 +22,6 @@
     <?php endwhile;?>
     <?php  endif; ?>
 
-    <?php get_sidebar();?>
+
         </section>
 <?php get_footer();?>
