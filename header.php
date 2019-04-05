5bb2d68cae34bd2eab1b00c9736af5d7e45a839d
info@codecreativenicaragua.com <info@codecreativenicaragua.com>
2019-04-04 19:11:32 -0600

    add favicon and css form

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php wp_title('|',true,'right') ?> <?php bloginfo('name') ?></title>
+    <link rel="icon" type="image/png" href="<?php echo RutaImagenes?>/Perfil-jehaann-manzanares.png" sizes="16x16">
     <?php wp_head(); ?>
 </head>
 <body>
