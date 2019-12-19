5a1dc485a4becccf193a9f870221bfef996cdf11
Jehaann-Manzanares <jehaann07carlos@gmail.com>
2019-12-19 17:58:15 -0600

    fix(sidebar): mejorando apariencia del sidebar, dividiendo en dos partes

--- sidebar.php
+++ sidebar.php
@@ -7,19 +7,19 @@ archivos con el sidebar del tema
 @since 1.0
 */
 ?>
-<aside class="sidebar-principal sticky">
+<aside class="sidebar-principal">
     <section class="user-data">
         <figure class="contenedor-img">
                 <img src= '<?php echo RutaImagenes?>/Perfil-jehaann-manzanares.png' alt="jehaann-manzanares.png">
         </figure>
         <div class="contenedor-info">
             <h1>Autor: <?php echo get_the_author(); ?></h1>
-            <p>Frontend/Backend developer</p>
+            <p>Soy informático de profesión especializado como:<br><br> 🔹Desarrollador FrontEnd<br>🔹Docente Creativo</p>
             
         </div>
         
     </section>
-    <section class="gidgets-seconds">
+    <section class="gidgets-seconds sticky">
         <?php if (is_active_sidebar('main-sidebar')) {
 
             dynamic_sidebar('main-sidebar');
