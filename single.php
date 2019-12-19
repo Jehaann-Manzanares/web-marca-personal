483fa7cde3716b2f5ae6982017bae8eaf1d3b133
Jehaann-Manzanares <jehaann07carlos@gmail.com>
2019-12-19 17:03:03 -0600

    fix(single.php): agregando imagen de cabecera de cada post de forma dinamica

--- single.php
+++ single.php
@@ -1,9 +1,15 @@
 <?php get_header();?>
 
     <?php if (have_posts() ): while ( have_posts() ): the_post(); ?>
-        <section class="container-blog-image" style="background-image: url('<?php echo RutaImagenes ?>/main-slider.png')">
+    <?php if (has_post_thumbnail()): ?>    
+       <?php $banner_article = wp_get_attachment_image_src(get_post_thumbnail_id(),'full');?>
+        <section class="container-blog-image" style="background-image: url('<?php echo banner_article[0]; ?>/main-slider.png')">
 
         </section>
+    <?php else:?>
+        <section class="container-blog-image" style="background-image: url('<?php echo RutaImagenes ?>/main-slider.png')">
+        </section>
+    <?php endif?>
         <section class="content-post">
             <article <?php post_class ('article single'); ?> id="post-<?php the_ID();?>">
                 <div class="article-content">
