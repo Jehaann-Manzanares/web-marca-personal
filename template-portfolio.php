2999e7d39fc0b403fd4977bebf0eb36ef0bf824d
Jehaann-Manzanares <jehaann07carlos@gmail.com>
2020-01-04 02:58:13 -0600

    feat(template-portfolio):agregando estructura del template y estilos

---
+++ template-portfolio.php
@@ -0,0 +1,80 @@
+<?php
+
+/** Template name: Portfolio
+* PAGINA DE PORTFOLIO DE PROYECTOS
+*@package wordpress
+*@subpackage jehaann
+*@since 1.0
+*/
+?>
+
+<?php get_header();?>
+    <section class="section__portfolio">
+        <h1 class="titles">Hey! bienvenido a mi portfolio</h1>
+        <h2 class="subtitle">FrontEnd</h2>
+        <div class="container__portfolio">
+            <div class="scroll__portfolio">
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+            </div>
+        </div>
+        <h2 class="subtitle">Web Design</h2>
+        <div class="container__portfolio">
+            <div class="scroll__portfolio">
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+                <article class="proyecto">
+                    <figure class="container__proyecto">
+                        <img class="proyecto__img"  src="<?php echo RutaImagenes?>/TechTing-Training-and-Consulting.png" alt="Proyecto Web design - techting"> 
+                    </figure>
+                </article>
+            </div>
+        </div>
+    </section>
+<?php get_footer();?>
